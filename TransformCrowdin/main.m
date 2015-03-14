9f92a024a914bd60d7ab18ca48aacd33b518e966
luohui8891 <luohui8891@gmail.com>
2015-03-14 22:49:55 +0800

    Initial Commit

---
+++ TransformCrowdin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TransformCrowdin
+//
+//  Created by LuoHui on 15/3/14.
+//  Copyright (c) 2015年 Andy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
